3e14359d94fd100a3607327f184d25113d717890
Minji Kim <minji_0801@naver.com>
2022-04-29 13:02:23 +0900

    Xcode 프로젝트 생성

---
+++ MemoUITests/MemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MemoUITestsLaunchTests.swift
+//  MemoUITests
+//
+//  Created by 김민지 on 2022/04/29.
+//
+
+import XCTest
+
+class MemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
